6c4e57556cb6621cf0cba02803037b8d91c87660
countchrisdo <crburwell@yahoo.com>
2021-08-06 13:25:05 -0400

    send-request refactor

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,4 +1,5 @@
 import { getToken } from './users-service';
+import sendRequest from './send-request';
 
 const BASE_URL = '/api/users';
 
@@ -12,28 +13,4 @@ export function login(credentials) {
 
 export function checkToken() {
   return sendRequest(`${BASE_URL}/check-token`);
-}
-
-/*--- Helper Functions ---*/
-
-async function sendRequest(url, method = 'GET', payload = null) {
-  // Fetch accepts an options object as the 2nd argument
-  // used to include a data payload, set headers, etc. 
-  const options = { method };
-  if (payload) {
-    options.headers = { 'Content-Type': 'application/json' };
-    options.body = JSON.stringify(payload);
-  }
-  const token = getToken();
-  if (token) {
-    // Ensure the headers object exists
-    options.headers = options.headers || {};
-    // Add token to an Authorization header
-    // Prefacing with 'Bearer' is recommended in the HTTP specification
-    options.headers.Authorization = `Bearer ${token}`;
-  }
-  const res = await fetch(url, options);
-  // res.ok will be false if the status code set to 4xx in the controller action
-  if (res.ok) return res.json();
-  throw new Error('Bad Request');
 }
