d7bdf5f7a20b88e2094f614fd5665d463d0ee7d2
countchrisdo <crburwell@yahoo.com>
2021-08-11 14:48:15 -0400

    Creating post grid

--- src/components/Moodboard/Moodboard.jsx
+++ src/components/Moodboard/Moodboard.jsx
@@ -1,10 +1,24 @@
-import "./Moodboard.css"
-
+import "./Moodboard.css";
 
 export default function Moodboard() {
-    return (
-        <div className="mood">
-            test
+  return (
+    <div className="mood">
+      <img
+        className="postImg"
+        src="https://cdn.dribbble.com/users/2171669/screenshots/5911127/image.png?compress=1&resize=800x600"
+      />
+
+      <div className="boardInfo">
+        <div className="boardCats">
+          <span className="boardCat">Music</span>
+          <span className="boardCat">Life</span>
         </div>
-    )
+        <span className="boardTitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </span>
+        <hr />
+        <span className="boardDate">1 Hour Ago</span>
+      </div>
+    </div>
+  );
 }
